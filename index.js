32aebc4f3d37289be70a9dda6582e7e9d4686098
MrAMB312 <a.blomarz@gmail.com>
2024-10-05 19:33:52 -0500

    complete phase 1 scope lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {    
+    return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const changeLeastFavoriteCustomer = 'charles';
+
+function changeLeastFavoriteCustomer() {
+    const changeLeastFavoriteCustomer = 'richard';
+}
